071c9d8bfdb39684732c602a5c55773f0b001a8d
ShipsMerlin <merlin@cs.ship.edu>
2014-04-23 04:58:54 -0400

    White background on image of water

--- Wonderfall/src/ImagePanel.java
+++ Wonderfall/src/ImagePanel.java
@@ -1,10 +1,16 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 class ImagePanel extends JPanel
@@ -15,6 +21,7 @@ class ImagePanel extends JPanel
 	 */
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Drawable> drawables;
+	private int totalWidth;
 
 	public ImagePanel()
 	{
@@ -28,7 +35,7 @@ class ImagePanel extends JPanel
 		drawables.add(new DrawableString(stringToPrint, font));
 		this.repaint();
 	}
-	
+
 	public void addImage(DrawableIcon img)
 	{
 		drawables.add(img);
@@ -39,21 +46,25 @@ class ImagePanel extends JPanel
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		int totalWidth = getWidthOfAllDrawables(g);
+		g.setColor(Color.WHITE);
+		totalWidth = getWidthOfAllDrawables(g);
 		this.setPreferredSize(new Dimension(32, totalWidth));
 		this.setMaximumSize(new Dimension(32, totalWidth));
 		this.setMinimumSize(new Dimension(32, totalWidth));
+		g.fillRect(this.getParent().getWidth()/2, 0, 32, totalWidth);
+		g.setColor(Color.BLACK);
 		int whereWeAre = 0;
 		Graphics2D g2 = (Graphics2D) g;
 		AffineTransform orig = g2.getTransform();
 		g2.rotate(-Math.PI / 2, totalWidth / 2, totalWidth / 2);
 		g2.translate(0, this.getParent().getWidth() / 2);
-		for(int i=0;i<drawables.size();i++)
-		{	
+		for (int i = 0; i < drawables.size(); i++)
+		{
 			int drawableWidth = drawables.get(i).getWidth(g);
-			drawables.get(i).drawYourself(g, whereWeAre);	
+			drawables.get(i).drawYourself(g, whereWeAre);
 			whereWeAre = whereWeAre + drawableWidth;
 		}
+
 		g2.setTransform(orig);
 		revalidate();
 		// BufferedImage img = new BufferedImage(super.getWidth(),
@@ -66,17 +77,46 @@ class ImagePanel extends JPanel
 	private int getWidthOfAllDrawables(Graphics g)
 	{
 		int totalWidth = 0;
-		for(int i=0;i<drawables.size();i++)
+		for (int i = 0; i < drawables.size(); i++)
 		{
 			totalWidth = totalWidth + drawables.get(i).getWidth(g);
 		}
 		return totalWidth;
 	}
 
-	public void getImageAsBits()
+	public boolean[][] getImageAsBits()
 	{
-		// TODO Auto-generated method stub
-		
+		boolean[][] bits = new boolean[totalWidth][32];
+
+		BufferedImage image = new BufferedImage(32, totalWidth,
+				BufferedImage.TYPE_USHORT_GRAY);
+		paint(image.getGraphics());
+		File outputfile = new File("saved.png");
+	    try
+		{
+			ImageIO.write(image, "png", outputfile);
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Raster raster = image.getRaster();
+		for (int col = 0; col < image.getWidth(); col++)
+		{
+			for (int row = 0; row < 32; row++)
+			{
+				int rgb = raster.getSample(col, row, 0);
+				System.out.print(rgb + " ");
+				
+				if (rgb == 0)
+				{
+					bits[row][col] = true;
+				}			
+			}
+			System.out.println();
+		}
+
+		return bits;
 	}
 
 }
